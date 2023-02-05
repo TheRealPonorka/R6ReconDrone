2172bc73063c7dd932c5b97360eac9a455276a9c
TheRealPonorka <124375719+TheRealPonorka@users.noreply.github.com>
2023-02-05 03:30:48 +0100

    Update handlers.h

--- handlers.h
+++ handlers.h
@@ -19,11 +19,19 @@ esp_err_t ledon_handler(httpd_req_t *req);
 
 esp_err_t ledoff_handler(httpd_req_t *req);
 
-esp_err_t ledGREEN_handler(httpd_req_t *req);
+esp_err_t ledDEFAULT_handler(httpd_req_t *req);
 
 esp_err_t ledRED_handler(httpd_req_t *req);
 
 esp_err_t ledYELLOW_handler(httpd_req_t *req);
+
+esp_err_t ledBLUE_handler(httpd_req_t *req);
+
+esp_err_t ledPURPLE_handler(httpd_req_t *req);
+
+esp_err_t ledORANGE_handler(httpd_req_t *req);
+
+esp_err_t rgbOFF_handler(httpd_req_t *req);
 
 byte RED;
 byte GREEN;
